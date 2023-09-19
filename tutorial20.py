4765b5e52fae6bf1f5353bae33d0c0fcfdcc3578
Shakyo choudhury <94360466+Shakyo-choudhury@users.noreply.github.com>
2023-09-19 22:48:44 +0530

    Add files via upload

---
+++ tutorial20.py
@@ -0,0 +1,20 @@
+# break statements
+
+# for looop
+for i in range(5):
+    if i == 3:
+        break
+    print(i)
+
+
+# program to find first 5 multiples of 6
+# while loop
+i = 1
+
+while i <= 10:
+    print('6 * ',(i), '=',6 * i)
+
+    if i >= 5:
+        break
+    
+    i = i + 1
